4ddcfd7d286628f84699a183e1122e4dc3eed071
cabelosglam <cabelosglam@gmail.com>
2026-03-31 00:22:04 -0300

    Atualizar o build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-pip install -r requirements.txt && python -m playwright install chromium && python -m playwright install-deps chromium
+pip install -r requirements.txt && python -m playwright install chromium
